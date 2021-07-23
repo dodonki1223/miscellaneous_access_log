acad22987b1c6f7bd1f3bdde706a6a39e93cc0f6
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-07-24 02:03:16 +0900

    :sparkles: codebuild で継続的に apply できる仕組みを作成

---
+++ codebuild.tf
@@ -0,0 +1,81 @@
+module "continuous_apply_codebuild_role" {
+  source     = "./modules/iam_role"
+  name       = "continuous-apply"
+  identifier = "codebuild.amazonaws.com"
+  policy     = data.aws_iam_policy.administrator_access.policy
+}
+
+data "aws_iam_policy" "administrator_access" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_codebuild_project" "continuous_apply" {
+  name         = "continuous-apply"
+  service_role = module.continuous_apply_codebuild_role.iam_role_arn
+  build_timeout = "5"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:1.0.3"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/dodonki1223/miscellaneous_access_log.git"
+    git_clone_depth = 1
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws codebuild import-source-credentials \
+        --server-type GITHUB \
+        --auth-type PERSONAL_ACCESS_TOKEN \
+        --token $GITHUB_TOKEN \
+        --profile terraform
+    EOT
+
+    environment = {
+      GITHUB_TOKEN = data.aws_ssm_parameter.github_token.value
+    }
+  }
+}
+
+data "aws_ssm_parameter" "github_token" {
+  name = "/continuous_apply/github_token"
+}
+
+resource "aws_codebuild_webhook" "continuous_apply" {
+  project_name = aws_codebuild_project.continuous_apply.name
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_CREATED"
+    }
+  }
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_UPDATED"
+    }
+  }
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "HEAD_REF"
+      pattern = "main"
+    }
+  }
+}
---
+++ network.tf
@@ -0,0 +1,3 @@
+resource "aws_vpc" "continuous_apply" {
+  cidr_block = "192.168.0.0/16"
+}
