68de15221734c62b726d6ac5000b7b5e126e2231
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-07-26 09:27:15 +0900

    :construction: 複数 component がデプロイされるか試す

---
+++ components/ec2/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.0.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.51.0"
+    }
+  }
+  backend "s3" {
+    region  = "ap-northeast-1"
+    encrypt = true
+    bucket  = "miscellaneous-access-log"
+    key     = "ec2/terraform.tfstate"
+    profile = "terraform"
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "terraform"
+}
--- components/hoge/main.tf
+++ components/hoge/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "example123"
+    Name = "hoge123"
   }
 }
