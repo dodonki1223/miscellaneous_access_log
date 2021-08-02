2d2ffec0a2ec2eef45855f1c9b5c0d38aa944fd6
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-08-02 09:21:29 +0900

    :sparkles: フォーマットチェックの CI を追加する

--- components/deployer/scripts/apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# 対象の components のディレクトリへ移動
-cd ${CODEBUILD_SRC_DIR}/components/$1
-
-# デプロイの実行
-terraform init -input=false -no-color
-terraform apply -input=false -no-color -auto-approve
--- components/deployer/scripts/plan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# 対象の components のディレクトリへ移動
-cd ${CODEBUILD_SRC_DIR}/components/$1
-
-# デプロイ計画の実行
-terraform init -input=false -no-color
-terraform plan -input=false -no-color
